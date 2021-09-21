e6901b886c6c3cd138fdad210fdd940c09cc29ac
blurryfac3e <bryanxreyes@gmail.com>
2021-09-22 01:39:07 +0800

    Footer gradient convert to png

--- index.php
+++ index.php
@@ -265,7 +265,7 @@ get_header(); ?>
     </section>
     <section id="footer_cta" class="footer-cta relative">
         <div class="blob-svg absolute bottom-0">
-            <svg class="w-full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1920" height="950.39" viewBox="0 0 1920 950.39">
+            <!-- <svg class="w-full" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="1920" height="950.39" viewBox="0 0 1920 950.39">
             <defs>
                 <style>
                 .cls-1 {
@@ -285,7 +285,8 @@ get_header(); ?>
                 <path id="BG" class="cls-1" d="M2345.06,9733.25l-111.03-603.73-46.47-2.94s-61.2-70.59-130.12-235.39c-55.17-129.94-112.99-253.87-175.06-374.67-30.23-58.83-76.41-137.92-155.12-210.96-95.32-88.44-194.98-134.68-261.51-151.25-170.63-42.5-324.89,7.22-443.1,53.18-177.962,69.21-396.215,181.26-458.083,218.74Q343.62,8560.09,90.427,8667.89A2973.515,2973.515,0,0,1-412.58,8892.85c-221.621,75.52-483.193,132.08-802.14,159.55a3812.308,3812.308,0,0,1-517.95,9.22L-1499.32,10445l3808.58-642.39Z" style="stroke: inherit; filter: none; fill: inherit"/>
             </g>
             <use transform="translate(0 -8132.61)" xlink:href="#BG" style="stroke: #fff; filter: none; fill: none"/>
-            </svg>
+            </svg> -->
+            <img src="<?php echo get_template_directory_uri() . '/dist/img/footer-gradient1.png' ?>" alt="blob" class="footer-gradient">
         </div>
         <div class="container relative">
             <img src="<?php echo get_template_directory_uri() . '/dist/img/glitters8-footer.png' ?>" alt="glitter" class="glitter glitters8-footer">
